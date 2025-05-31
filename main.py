182e4020bf2fbc85d3818c4b53de0bca92e53cde
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-05-31 17:25:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -217,5 +217,5 @@ async def predict(image: UploadFile = File(...)):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
