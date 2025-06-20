c1e8f481bdc3a08e0e38a8ef063a077743beb230
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-06-20 13:23:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import tempfile
 # Paramètres de connexion blob storage
 AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=stockaccountp8;AccountKey=flae3B4NIMDm7xc1N3pmP84VgN+zqnM0+HsGw/Y+OqhfomqVLftO9jy4J5r2aIn+eccsB1G8A147+AStRvQ6TA==;EndpointSuffix=core.windows.net"
 CONTAINER_NAME = "projet8"
-BLOB_NAME = "unet_vgg16_aug_epoch50_model.weights.h5"
+BLOB_NAME = "model/unet_vgg16_aug_epoch50_model.weights.h5"
 LOCAL_WEIGHTS_PATH = os.path.join("APImodel", "APImodel.weights.h5")
 
 id_to_color = {
